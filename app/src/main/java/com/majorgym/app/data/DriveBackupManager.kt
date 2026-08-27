7bf72734a2d6a16f45bd255dd4705b4a13331427
sameer <fitsam3434@gmail.com>
2026-08-27 12:01:33 +0530

    Fix: replace deprecated AndroidHttp with NetHttpTransport (unresolved reference)

--- app/src/main/java/com/majorgym/app/data/DriveBackupManager.kt
+++ app/src/main/java/com/majorgym/app/data/DriveBackupManager.kt
@@ -9,10 +9,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.Scope
-import com.google.api.client.extensions.android.http.AndroidHttp
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.client.http.ByteArrayContent
+import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
@@ -76,7 +76,7 @@ class DriveBackupManager(private val context: Context) {
     private fun driveService(account: GoogleSignInAccount): Drive {
         val credential = GoogleAccountCredential.usingOAuth2(context, listOf(DriveScopes.DRIVE_FILE))
         credential.selectedAccount = account.account
-        return Drive.Builder(AndroidHttp.newCompatibleTransport(), GsonFactory.getDefaultInstance(), credential)
+        return Drive.Builder(NetHttpTransport(), GsonFactory.getDefaultInstance(), credential)
             .setApplicationName("MajorGym")
             .build()
     }
