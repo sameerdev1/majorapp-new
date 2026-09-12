108c733002e4238213634d31423856203761b025
sameer <fitsam3434@gmail.com>
2026-09-12 12:31:05 +0530

    Fix missing clickable import in SyncScreen

--- app/src/main/java/com/majorgym/app/ui/SyncScreen.kt
+++ app/src/main/java/com/majorgym/app/ui/SyncScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
